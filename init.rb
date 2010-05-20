e5b35ab268b215da6962f1d26f56010eba9e2503
Miles Georgi <azimux@gmail.com>
2010-05-19 18:01:13 -0700

    A more sophisticated truncation: the number of chopped off characters is now appended to the end of the index name.  This reduces (but does not eliminate) the likelihood of collisions.

--- init.rb
+++ init.rb
@@ -3,6 +3,23 @@ ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
   #index names to 63 characters. Since some do not, this override makes
   #sure that the truncate occurs regardless of PostgreSQL driver.
   def index_name(table, columns)
-    super(table, columns)[0..62]
+    name = super(table, columns)
+    if name.size <= 63
+      name
+    else
+      #replace the end of the string with the size of the chopped off part,
+      #to reduce the likelyhood of collisions
+      ax_truncate_index_name(name)
+    end
+  end
+
+  def ax_truncate_index_name index_name
+    if index_name =~ /^([\w\d_]{63})([\w\d_]+)$/
+      size = $2.size.to_s
+
+      $1[0..(62 - size.size)] + size
+    else
+      index_name
+    end
   end
 end
