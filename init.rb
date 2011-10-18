82486c102a3b1cde6714e4b18b0ce600054ff879
Miles Georgi <azimux@gmail.com>
2011-10-17 20:00:44 -0700

    For some reason, ActiveRecord::ConnectionAdapters::PostgreSQLAdapter is throwing a NameError, although it works if I call ActiveRecord::Base.connection first.

--- init.rb
+++ init.rb
@@ -1,4 +1,8 @@
-ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
+# I am not sure why, but we get a NameError on ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
+# which makes no sense at all to me.rake
+raise unless ActiveRecord::Base.connection.class.name =~ /PostgreSQLAdapter/
+
+ActiveRecord::Base.connection.class.class_eval do
   #There are PostgreSQL drivers/versions of drivers that truncate
   #index names to 63 characters. Since some do not, this override makes
   #sure that the truncate occurs regardless of PostgreSQL driver.
