f21179128bbb24c90e053a2d345146d9087192cb
Miles Georgi <azimux@gmail.com>
2013-07-01 12:46:04 -0700

    Patch new schema plus index name generating method.

--- init.rb
+++ init.rb
@@ -27,3 +27,20 @@ ActiveRecord::Base.connection.class.class_eval do
     end
   end
 end
+
+# Ugg, more monkey patching
+# let's repair broken schema_plus
+
+if defined?(SchemaPlus::ActiveRecord::ColumnOptionsHandler)
+  module SchemaPlus::ActiveRecord
+    module ColumnOptionsHandler
+      def auto_index_name_with_fix_psql_index(*args)
+        index_name = auto_index_name_without_fix_psql_index(*args)
+        ActiveRecord::Base.connection.ax_truncate_index_name(index_name)
+      end
+      alias_method_chain :auto_index_name, :fix_psql_index
+    end
+  end
+else
+  raise "no schema plus??"
+end
